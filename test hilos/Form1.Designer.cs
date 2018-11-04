d2cd263e0397e9fd02b9f2f2cd96035b7185e319
josé Vicente Dochao Fernández <jvdochaofernandez@gmail.com>
2018-11-04 21:12:00 +0100

    Carga proyecto test hilos

---
+++ test hilos/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿using System.Threading;
+
+
+namespace test_hilos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnInitProc = new System.Windows.Forms.Button();
+            this.BtShowMessage = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LblProgres = new System.Windows.Forms.Label();
+            this.PrbProgress = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // BtnInitProc
+            // 
+            this.BtnInitProc.Location = new System.Drawing.Point(68, 83);
+            this.BtnInitProc.Name = "BtnInitProc";
+            this.BtnInitProc.Size = new System.Drawing.Size(104, 23);
+            this.BtnInitProc.TabIndex = 0;
+            this.BtnInitProc.Text = "Init process";
+            this.BtnInitProc.UseVisualStyleBackColor = true;
+            this.BtnInitProc.Click += new System.EventHandler(this.BtnInitProc_Click);
+            // 
+            // BtShowMessage
+            // 
+            this.BtShowMessage.Location = new System.Drawing.Point(68, 138);
+            this.BtShowMessage.Name = "BtShowMessage";
+            this.BtShowMessage.Size = new System.Drawing.Size(104, 23);
+            this.BtShowMessage.TabIndex = 1;
+            this.BtShowMessage.Text = "Show message";
+            this.BtShowMessage.UseVisualStyleBackColor = true;
+            this.BtShowMessage.Click += new System.EventHandler(this.BtShowMessage_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Thread tutorial";
+            // 
+            // LblProgres
+            // 
+            this.LblProgres.AutoSize = true;
+            this.LblProgres.Location = new System.Drawing.Point(68, 193);
+            this.LblProgres.Name = "LblProgres";
+            this.LblProgres.Size = new System.Drawing.Size(49, 13);
+            this.LblProgres.TabIndex = 3;
+            this.LblProgres.Text = "Progreso";
+            // 
+            // PrbProgress
+            // 
+            this.PrbProgress.Location = new System.Drawing.Point(68, 245);
+            this.PrbProgress.Name = "PrbProgress";
+            this.PrbProgress.Size = new System.Drawing.Size(100, 23);
+            this.PrbProgress.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(238, 280);
+            this.Controls.Add(this.PrbProgress);
+            this.Controls.Add(this.LblProgres);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtShowMessage);
+            this.Controls.Add(this.BtnInitProc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnInitProc;
+        private System.Windows.Forms.Button BtShowMessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LblProgres;
+        private System.Windows.Forms.ProgressBar PrbProgress;
+    }
+}
+
---
+++ test hilos/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace test_hilos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnInitProc_Click(object sender, EventArgs e)
+        {
+
+            //Creamos el delegado 
+            ThreadStart delegado = new ThreadStart(CorrerProceso);
+            //Creamos la instancia del hilo 
+            Thread hilo = new Thread(delegado);
+            //Iniciamos el hilo 
+            hilo.Start();
+            
+        }
+
+        private void CorrerProceso()
+        {
+            ////Hacer que se tarde 10000 milisegundos (10 segundos) 
+            //Thread.Sleep(10000);
+            //MessageBox.Show("Proceso finalizado");
+
+            CambiarProgreso("Iniciando proceso...", 0);
+            //hagamos un ciclo que se repita 100 veces 
+            for (int i = 0; i < 100; i++)
+            {
+                //esperaremos medio segundo en cada iteración 
+                Thread.Sleep(500);
+                //notificamos el avance al usuario 
+                CambiarProgreso(string.Format("Posición {0}...", i), i);
+            }
+            CambiarProgreso("Completado :D", 100);
+            MessageBox.Show("Proceso finalizado");
+        }
+
+        delegate void CambiarProgresoDelegado(string texto, int valor);
+
+        private void CambiarProgreso(string texto, int valor)
+        {
+            if (this.InvokeRequired) //preguntamos si la llamada se hace desde un hilo 
+            {
+                //si es así entonces volvemos a llamar a CambiarProgreso pero esta vez a través del delegado 
+                //instanciamos el delegado indicandole el método que va a ejecutar 
+                CambiarProgresoDelegado delegado = new CambiarProgresoDelegado(CambiarProgreso);
+                //ya que el delegado invocará a CambiarProgreso debemos indicarle los parámetros 
+                object[] parametros = new object[] { texto, valor };
+                //invocamos el método a través del mismo contexto del formulario (this) y enviamos los parámetros 
+                this.Invoke(delegado, parametros);
+            }
+            else
+            {
+                //en caso contrario, se realiza el llamado a los controles 
+                LblProgres.Text = texto;
+                PrbProgress.Value = valor;
+            }
+        }
+
+        
+        private void BtShowMessage_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola Edwin");
+        }
+    }
+}
